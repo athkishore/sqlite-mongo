577a1dfe34abcbee7b7ad33586fc90e0de4816ae
kishore <kishore.asok@gmail.com>
2025-11-04 19:30:00 +0530

    add more logic in journey part 2

--- journey/02-eavesdropping-on-wire/src/index.ts
+++ journey/02-eavesdropping-on-wire/src/index.ts
@@ -20,16 +20,45 @@ async function handleNewConnection(clientSock: Socket) {
     () => console.log('created proxy connection to mongod server')
   );
 
+  let c2sBuf = Buffer.alloc(0);
+  let s2cBuf = Buffer.alloc(0);
+
   // register a callback to forward to server
   // any data sent from client
   clientSock.on('data', (data) => {
     serverSock.write(data);
+    c2sBuf = Buffer.concat([c2sBuf, data]);
+    processBuffer(c2sBuf); // to be defined
   });
 
   // register a callback to forward to client
   // any data sent from server
   serverSock.on('data', (data) => {
     clientSock.write(data);
+    s2cBuf = Buffer.concat([s2cBuf, data]);
+    processBuffer(s2cBuf);
+  });
+
+  clientSock.on('error', (error) => {
+    console.log(`client socket error on port ${clientSock.remotePort}:`, error.message);
+    clientSock.destroy();
+    serverSock.destroy();
+  });
+
+  serverSock.on('error', (error) => {
+    console.log('server socket error:', error.message);
+    clientSock.destroy();
+    serverSock.destroy();
+  });
+
+  clientSock.on('close', () => {
+    console.log(`client closed connection on port ${clientSock.remotePort}`);
+    serverSock.destroy();
+  });
+
+  serverSock.on('close', () => {
+    console.log(`server closed connection`);
+    clientSock.destroy();
   });
 }
 
@@ -43,3 +72,7 @@ server.listen(LISTEN_PORT, LISTEN_PORT, () => {
     `${MONGOD_HOST}:${MONGOD_PORT}`,
   );
 })
+
+function processBuffer(buf: Buffer) {
+  
+}

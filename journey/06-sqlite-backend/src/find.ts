dcc4e924884b070327f0fe49d869e8c53ba2cb83
kishore <kishore.asok@gmail.com>
2025-11-08 18:43:16 +0530

    experiment with filter parse tree structures

--- journey/06-sqlite-backend/src/find.ts
+++ journey/06-sqlite-backend/src/find.ts
@@ -6,10 +6,68 @@ const db = new Database(DB_PATH);
 const collection = process.argv[2];
 if (!collection) {
   console.error('Collection name is required');
+  process.exit(1);
 }
 
-const filter = process.argv[3];
-if (!filter) {
+const filterJSON = process.argv[3];
+if (!filterJSON) {
   console.error('Filter is required');
+  process.exit(1);
+}
+
+const { error, filterParseTree } = parseFilter(filterJSON);
+if (error) console.error(error);
+if (filterParseTree) console.dir(filterParseTree, { depth: null });
+
+interface FilterParseTree {
+  operator: '$and',
+  operands: FilterParseNode[];
+}
+
+interface FilterParseNode {
+  operator: any;
+  operands: (FilterParseNode | string | number | boolean | BigInt | null)[];
+}
+
+function parseFilter(filterJSON: string): { error: Error | null, filterParseTree: FilterParseTree | null } {
+  try {
+    const filter = JSON.parse(filterJSON);
+    const elements = Object.entries(filter);
+
+    const operands: FilterParseNode[] = [];
+
+    for (const [key, value] of elements) {
+      if (
+        !key.match(/^\$/) 
+        && (
+          typeof value === 'string'
+          || typeof value === 'number'
+          || typeof value === 'bigint'
+          || typeof value === 'boolean'
+          || value === null
+        )
+      ) {
+        operands.push({
+          operator: '$eq',
+          operands: [{ operator: '$ref', operands: [key] }, value]
+        });
+      } else {
+        throw new Error('Operand type not supported yet');
+      }
+    }
+
+    return {
+      error: null,
+      filterParseTree: {
+        operator: '$and',
+        operands,
+      }
+    }
+  } catch(error) {
+    return {
+      error: error as Error,
+      filterParseTree: null,
+    };
+  }
 }
 
--- journey/06-sqlite-backend/src/insert-mongodb.ts
+++ journey/06-sqlite-backend/src/insert-mongodb.ts
@@ -2,18 +2,20 @@ import { MongoClient } from "mongodb";
 import fs from 'fs';
 
 const DB_URI = 'mongodb://127.0.0.1:27017';
-const JSON_DIR = 'data/json';
+const JSON_DIR = 'data/json/cricket-data';
 
 async function main() {
   const client = await MongoClient.connect(DB_URI);
   const db = client.db('cricket');
   const matchesCollection = db.collection('matches');
 
-  const jsonFiles = fs.readdirSync(JSON_DIR);
+  const jsonFiles = fs.readdirSync(JSON_DIR).filter(el => el.match(/json$/));
   for (const jsonFile of jsonFiles) {
     const doc = JSON.parse(fs.readFileSync(`${JSON_DIR}/${jsonFile}`, 'utf-8'));
     await matchesCollection.insertOne(doc);
   }
+
+  await client.close();
 }
 
 main();

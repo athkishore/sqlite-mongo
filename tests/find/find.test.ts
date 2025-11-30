f6ce10e269b839e6e36d202a6486d030df28c9f3
kishore <kishore.asok@gmail.com>
2025-11-30 15:52:55 +0530

    Refactor tests to abstract the boilerplate

--- tests/find/find.test.ts
+++ tests/find/find.test.ts
@@ -1,4 +1,5 @@
 import { generateAndExecuteSQLFromQueryIR } from "#sql-generator/index.js";
+import { parseFindCommand } from "#src/query-parser/find.js";
 import type { FindCommandIR } from "../../src/types.js";
 import assert from "assert";
 import Database from "better-sqlite3";
@@ -36,142 +37,213 @@ before(() => {
   }
 });
 
-describe('find command', () => {
-  it('fetches all documents with empty filter', () => {
-    const command: FindCommandIR = {
+type Test = {
+  type: 'test';
+  name: string;
+  input: { filter: Record<string, any>; };
+  expect: (resultDocs: Record<string, any>[], collnDocs: Record<string, any>[]) => boolean,
+};
+
+type Suite = {
+  type: 'suite';
+  name: string;
+  children: (Suite | Test)[];
+};
+
+const suite: Suite = {
+  type: 'suite',
+  name: 'find command',
+  children: [
+    {
+      type: 'test',
+      name: 'fetches all documents with empty filter',
+      input: {
+        filter: {}
+      },
+      expect: (result, collection) => result.length === collection.length,
+    },
+    {
+      type: 'suite',
+      name: 'filters a document by top-level field',
+      children: [
+        {
+          type: 'suite',
+          name: 'string field',
+          children: [
+            {
+              type: 'test',
+              name: 'using $eq',
+              input: {
+                filter: { username: 'user1' },
+              },
+              expect: (result) => result.length === 1
+                && result[0]!.username === 'user1',
+            },
+            {
+              type: 'test',
+              name: 'using $gt',
+              input: {
+                filter: { username: { $gt: 'user2' } },
+              },
+              expect: (result) => result.length === 1
+                && result[0]!.username === 'user3',
+            },
+            {
+              type: 'test',
+              name: 'using $gte',
+              input: {
+                filter: { username: { $gte: 'user2' } },
+              },
+              expect: (result) => {
+                const usernames = result.map(el => el.username);
+                return usernames.length === 2
+                  && usernames.includes('user2')
+                  && usernames.includes('user3');
+              },
+            }
+          ]
+        },
+        {
+          type: 'suite',
+          name: 'array field',
+          children: [
+            {
+              type: 'test',
+              name: 'primitive value - using $eq',
+              input: {
+                filter: { follows: 'user2' },
+              },
+              expect: (result) => result.length === 2,
+            },
+            {
+              type: 'test',
+              name: 'array value - using $eq - returns exact match',
+              input: {
+                filter: { follows: ['user3', 'user2'] }
+              },
+              expect: result => result.length === 1,
+            },
+            {
+              type: 'test',
+              name: 'array value - using $eq - does not return inexact match',
+              input: {
+                filter: { follows: ['user2', 'user3'] },
+              },
+              expect: result => result.length === 0,
+            },
+          ],
+        },
+        {
+          type: 'suite',
+          name: 'null field',
+          children: [
+            /*  
+            *   This behaviour is different from MongoDB 
+            *   In MongoDB, it also returns documents where
+            *   the field doesn't exist
+            */
+            {
+              type: 'test',
+              name: 'using $eq',
+              input: {
+                filter: { homepage: null },
+              },
+              expect: result => result.length === 1
+                && result[0]!.username === 'user1',
+            },
+            {
+              type: 'test',
+              name: 'using $ne',
+              input: {
+                filter: { homepage: { $ne: null } },
+              },
+              expect: result => result.length === 1
+                && result[0]!.username === 'user2',
+            }
+          ]
+        },
+        // {
+        //   type: 'suite',
+        //   name: 'using $exists',
+        //   children: [
+        //     {
+        //       type: 'test',
+        //       name: 'field exists',
+        //       input: {
+        //         filter: { homepage: { $exists: true } },
+        //       },
+        //       expect: result => result.length === 2,
+        //     },
+        //     {
+        //       type: 'test',
+        //       name: 'field does not exist',
+        //       input: {
+        //         filter: { homepage: { $exists: false } },
+        //       },
+        //       expect: result => result.length === 1,
+        //     },
+        //   ],
+        // }
+      ]
+    },
+    {
+      type: 'suite',
+      name: 'filters a document by a nested field inside an object',
+      children: [
+        {
+          type: 'test',
+          name: 'using $eq',
+          input: {
+            filter: { 'address.city': 'Bangalore' }
+          },
+          expect: result => result.length === 1
+            && result[0]!.username === 'user1',
+        }
+      ]
+    },
+    {
+      type: 'suite',
+      name: 'filters a document by a nested field in an array element',
+      children: [
+        {
+          type: 'test',
+          name: 'using $eq',
+          input: {
+            filter: { 'phones.type': 'mobile' },
+          },
+          expect: result => result.length === 2,
+        }
+      ],
+    }
+  ]
+};
+
+function executeTest(test: Test) {
+  it(test.name, () => {
+    const comamndIR = parseFindCommand({
       command: 'find',
       database: 'test',
-      collection: seedCollections[0].collection,
-      filter: { operator: '$and', operands: [] },
-    };
-
-    const sqlResult = generateAndExecuteSQLFromQueryIR(command, db);
+      collection: 'users',
+      filter: test.input.filter,
+    });
+  
+    const sqlResult = generateAndExecuteSQLFromQueryIR(comamndIR, db);
     const sqlResultDocuments = sqlResult.cursor.firstBatch;
+  
+    assert(test.expect(sqlResultDocuments, seedCollections[0].documents));
+  });
+}
 
-    const documents = seedCollections[0].documents;
-
-    assert.equal(documents.length, sqlResultDocuments.length);
-    for (const document of documents) {
-      const resultDoc = sqlResultDocuments.find((el: any) => el.username === document.username);
-      delete resultDoc['_id'];
-
-      assert.equal(JSON.stringify(document), JSON.stringify(resultDoc));
+function executeSuite(suite: Suite) {
+  describe(suite.name, () => {
+    for (const child of suite.children) {
+      if (child.type === 'suite') {
+        executeSuite(child)
+      } else {
+        executeTest(child);
+      }
     }
-  });
+  })
+}
 
-  describe('filters a document by top-level field', () => {
-    describe('string', () => {
-      it('using $eq', () => {
-        const command: FindCommandIR = {
-          command: 'find',
-          database: 'test',
-          collection: seedCollections[0].collection,
-          filter: { operator: '$eq', operands: [{ $ref: 'username' }, 'user1']},
-        };
-    
-        const sqlResult = generateAndExecuteSQLFromQueryIR(command, db);
-        const sqlResultDocuments = sqlResult.cursor.firstBatch;
-    
-        assert.equal(sqlResultDocuments.length, 1);
-        assert.equal(sqlResultDocuments[0].username, 'user1');
-      });
-  
-      it('using $gt', () => {
-        const command: FindCommandIR = {
-          command: 'find',
-          database: 'test',
-          collection: seedCollections[0].collection,
-          filter: { operator: '$gt', operands: [{ $ref: 'username' }, 'user2' ] },
-        };
-  
-        const sqlResult = generateAndExecuteSQLFromQueryIR(command, db);
-        const sqlResultDocuments = sqlResult.cursor.firstBatch;
-  
-        assert.equal(sqlResultDocuments.length, 1);
-        assert.equal(sqlResultDocuments[0].username, 'user3');
-      });
-  
-      it('using $gte', () => {
-        const command: FindCommandIR = {
-          command: 'find',
-          database: 'test',
-          collection: seedCollections[0].collection,
-          filter: { operator: '$gte', operands: [{ $ref: 'username' }, 'user2'] },
-        };
-  
-        const sqlResult = generateAndExecuteSQLFromQueryIR(command, db);
-        const sqlResultDocuments = sqlResult.cursor.firstBatch;
-  
-        assert.equal(sqlResultDocuments.length, 2);
-        for (const document of sqlResultDocuments) {
-          assert(document.username >= 'user2');
-        }
-      });
-    });
-
-    describe('array', () => {
-      it('primitive value - using $eq', () => {
-        const command: FindCommandIR = {
-          command: 'find',
-          database: 'test',
-          collection: seedCollections[0].collection,
-          filter: { operator: '$eq', operands: [{ $ref: 'follows' }, 'user2'] },
-        };
-
-        const sqlResult = generateAndExecuteSQLFromQueryIR(command, db);
-        const sqlResultDocuments = sqlResult.cursor.firstBatch;
-
-        assert.equal(sqlResultDocuments.length, 2);
-      });
-
-      it('array value - using $eq - returns only exact matches', () => {
-        {
-          const command: FindCommandIR = {
-            command: 'find',
-            database: 'test',
-            collection: seedCollections[0].collection,
-            filter: { operator: '$eq', operands: [{ $ref: 'follows' }, ['user3', 'user2']] },
-          };
-  
-          const sqlResult = generateAndExecuteSQLFromQueryIR(command, db);
-          const sqlResultDocuments = sqlResult.cursor.firstBatch;
-
-          assert.equal(sqlResultDocuments.length, 1);
-        }
-      })
-    })
-  });
-
-  describe('filters a document by a nested field inside an object', () => {
-    it('using $eq', () => {
-      const command: FindCommandIR = {
-        command: 'find',
-        database: 'test',
-        collection: seedCollections[0].collection,
-        filter: { operator: '$eq', operands: [{ $ref: 'address.city' }, 'Bangalore']},
-      };
-
-      const sqlResult = generateAndExecuteSQLFromQueryIR(command, db);
-      const sqlResultDocuments = sqlResult.cursor.firstBatch;
-
-      assert.equal(sqlResultDocuments.length, 1);
-      assert.equal(sqlResultDocuments[0].username, 'user1');
-    });
-  });
-
-  describe('filters a document by a nested field in an array element', () => {
-    it('using $eq', () => {
-      const command: FindCommandIR = {
-        command: 'find',
-        database: 'test',
-        collection: seedCollections[0].collection,
-        filter: { operator: '$eq', operands: [{ $ref: 'phones.type' }, 'mobile'] },
-      };
-
-      const sqlResult = generateAndExecuteSQLFromQueryIR(command, db);
-      const sqlResultDocuments = sqlResult.cursor.firstBatch;
-
-      assert.equal(sqlResultDocuments.length, 2);
-    })
-  });
-})
+executeSuite(suite);

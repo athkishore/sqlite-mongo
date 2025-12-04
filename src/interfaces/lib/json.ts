c3e0f5595d414b1860b5cbd260aab5d82b835dcc
kishore <kishore.asok@gmail.com>
2025-12-04 21:09:48 +0530

    Complete BSON type handling implementation

--- src/interfaces/lib/json.ts
+++ src/interfaces/lib/json.ts
@@ -1,9 +1,10 @@
-import { Decimal128, ObjectId, Binary } from "bson";
+import { Decimal128, ObjectId, Binary, MinKey, MaxKey, BSONRegExp, Timestamp, EJSON } from "bson";
 
 export function stringifyToCustomJSON(value: any) {
   const orig_ObjectId_toJSON = ObjectId.prototype.toJSON;
   const orig_Date_toJSON = Date.prototype.toJSON;
   const orig_Binary_toJSON = Binary.prototype.toJSON;
+  const orig_Timestamp_toJSON = Timestamp.prototype.toJSON;
 
   ObjectId.prototype.toJSON = function(this: ObjectId) {
     return { $oid: this.toHexString() };
@@ -17,12 +18,38 @@ export function stringifyToCustomJSON(value: any) {
     return { $binary: { base64: orig_Binary_toJSON.apply(this), subType: this.sub_type.toString() } };
   } as any;
 
+  (MinKey.prototype as any).toJSON = function() {
+    return { $minKey: 1 };
+  };
+
+  (MaxKey.prototype as any).toJSON = function() {
+    return { $maxKey: 1 };
+  };
+
+  Timestamp.prototype.toJSON = function(this: Timestamp) {
+    return { $timestamp: { t: this.t, i: this.i } };
+  } as any;
+
   try {
-    return JSON.stringify(value);  
+    return JSON.stringify(value, (_, v) => {
+      if (v instanceof RegExp) {
+        return {
+          $regularExpression: {
+            pattern: v.source,
+            options: v.flags,
+          },
+        };
+      }
+
+      return v;
+    });  
   } finally {
     ObjectId.prototype.toJSON = orig_ObjectId_toJSON;
     Date.prototype.toJSON = orig_Date_toJSON;
     Binary.prototype.toJSON = orig_Binary_toJSON;
+    delete (MinKey.prototype as any).toJSON;
+    delete (MaxKey.prototype as any).toJSON;
+    Timestamp.prototype.toJSON = orig_Timestamp_toJSON;
   }
 }
 
@@ -41,6 +68,22 @@ export function parseFromCustomJSON(text: string) {
 
       if (value.$binary) {
         return new Binary(Buffer.from(value.$binary.base64, 'base64'), Number(value.$binary.subType));
+      }
+
+      if (value.$minKey) {
+        return new MinKey();
+      }
+
+      if (value.$maxKey) {
+        return new MaxKey();
+      }
+
+      if (value.$regularExpression) {
+        return new BSONRegExp(value.$regularExpression.pattern, value.$regularExpression.options);
+      }
+
+      if (value.$timestamp) {
+        return EJSON.parse(JSON.stringify(value));
       }
     }
     return value;

476ef64fbd75110863cf03e1954bb7a9df82a2ae
Kishore Athrasseri <kishore.asok@gmail.com>
2025-12-11 10:04:25 +0530

    Write function to get flat list of dotted include paths from IR

--- src/sql-generator/common/projection.ts
+++ src/sql-generator/common/projection.ts
@@ -1,5 +1,44 @@
-import type { ProjectionDocIR } from "#src/types.js";
+import type { ProjectionDocIR, ProjectionNodeIR } from "#src/types.js";
 
 export function getProjectionFragment(projection: ProjectionDocIR) {
-  return '';
+  const includePaths: string[] = [];
+
+  const { include } = projection;
+
+
+  let s = '';
+  s += `(\n`;
+  s += `  WITH\n`;
+  s += `  include_paths AS (\n`;
+
+  s += `),\n`;
+
+
+  s += `)`;
+
+
+  return s;
+}
+
+
+function getPathsFromIR(nodes: ProjectionNodeIR[]) {
+  const paths: string[] = [];
+
+  function walk(node: ProjectionNodeIR, parentPath: string) {
+    if (node.children.length === 0) {
+      paths.push(`${parentPath}${parentPath === '' ? '' : '.'}${node.path}`);
+      return;
+    }
+
+    for (const child of node.children) {
+      walk(child, `${parentPath}${parentPath === '' ? '' : '.'}${node.path}`);
+    }
+  }
+
+  for (const node of nodes) {
+    walk(node, '');
+  }
+
+  return paths;
+
 }

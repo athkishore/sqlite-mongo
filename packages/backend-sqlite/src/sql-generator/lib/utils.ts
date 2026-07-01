dd167f70e79f276ec8312350a181ef2c7d3de160
Kishore Athrasseri <kishore.asok@gmail.com>
2026-07-01 09:43:14 +0530

    feat(backend-sqlite): Partially implement udf for match

--- packages/backend-sqlite/src/sql-generator/lib/utils.ts
+++
@@ -1,4 +0,0 @@
-import debug from 'debug';
-export const logSql = debug('sqlite:sql');
-export const logSqlResult = debug('sqlite:result');
-export const logSqlExecTime = debug('sqlite:time');
--- packages/backend-sqlite/src/sql-generator/user-defined-functions/match.ts
+++ packages/backend-sqlite/src/sql-generator/user-defined-functions/match.ts
@@ -1,5 +1,69 @@
-import type { FilterNodeIR } from "@chikkadb/interfaces/command/types";
+import { DOC_LEVEL_FILTER_OPERATORS, FIELD_LEVEL_FILTER_OPERATORS, type FieldReference, type FilterNodeIR, type FilterNodeIR_DocLevel, type FilterNodeIR_FieldLevel, type Value } from "@chikkadb/interfaces/command/types";
 
-export function match(doc: Record<string, any>, filter: FilterNodeIR): 1 | 0 {
-  return doc['username'] === 'user2' ? 1 : 0;
+export function match(doc: Record<string, any>, filter: FilterNodeIR): boolean {
+  function traverseFilter(doc: any, filter: FilterNodeIR): boolean {
+    const { operator, operands } = filter;
+
+    if (
+      operator &&
+      FIELD_LEVEL_FILTER_OPERATORS.includes(operator as FilterNodeIR_FieldLevel['operator'])
+    ) {
+      const ref = (operands[0] as FieldReference);
+      const value = operands[1] as Value;
+      const op = operator as FilterNodeIR_FieldLevel['operator'];
+
+      const result = applyFieldLevelOperator({
+        doc,
+        ref,
+        value,
+        operator: op,
+      });
+
+      return result;
+    } else if (
+      operator
+      && DOC_LEVEL_FILTER_OPERATORS.includes(operator as FilterNodeIR_DocLevel['operator'])
+    ) {
+      const results = [];
+      for (const operand of operands) {
+        results.push(traverseFilter(doc, operand as FilterNodeIR));
+      }
+      if (operator === '$and') {
+        return results.reduce((acc, el) => acc && el, true);
+      } else if (operator === '$or') {
+        return results.reduce((acc, el) => acc || el, false);
+      } else if (operator === '$nor') {
+        return results.reduce((acc, el) => !(acc || el), false);
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+
+  
+  return traverseFilter(doc, filter);
+}
+
+function applyFieldLevelOperator({
+  doc,
+  ref,
+  value,
+  operator,
+}: {
+  doc: Record<string, any>;
+  ref: FieldReference;
+  value: Value;
+  operator: typeof FIELD_LEVEL_FILTER_OPERATORS[number];
+}): boolean {
+  switch(operator) {
+    case '$eq': {
+      if (typeof value === 'string') {
+        return true;
+      }
+    }
+  }
+  return false;
 }
